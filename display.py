99ea98096756fb14bc19f403afb758fc92418a26
Paul Veltheim <noreply>
2024-01-22 16:22:42 +0200

    updated description for display.py

---
+++ display.py
@@ -0,0 +1,12 @@
+## This module controls the display of different app states.
+## This includeds the following functionality:
+##  - display user home screen with worlds
+##  - display world screen with npc:s
+##  - display npc creator screen 
+##  - display npc avatar creator (npc creator screen)
+##  - display topic creator (npc creator screen)
+##  - display dialogue option creator (npc creator screen)
+##  - display dialogue creator (npc creator screen)
+##  - display play mode screen
+##  - display dialogue menu (play mode screen)
+##  - display npc (npc creator screen, world screen, play mode screen (with emotions))
