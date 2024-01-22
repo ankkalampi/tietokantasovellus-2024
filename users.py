0f73e3a6a30329b8974814da06eb2ee9946fce1e
Paul Veltheim <noreply>
2024-01-22 16:14:16 +0200

    updated description for users.py

---
+++ users.py
@@ -0,0 +1,7 @@
+## This module is handles user-related tasks. 
+## This includes the following functionality:
+##  - create and delete users
+##  - authenticate user
+##  - logout user
+##  - elevate or demote user privileges
+##  - denote current user id
