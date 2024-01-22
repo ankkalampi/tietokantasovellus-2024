00aa9b4cbc44224182f076d3a6c25a0522c9a738
Paul Veltheim <noreply>
2024-01-22 16:27:57 +0200

    updated description for world.py

---
+++ world.py
@@ -0,0 +1,9 @@
+## This module handles world (npc group) related tasks.
+## This includes the following functionality:
+##  - create and delete worlds
+##  - load world based on world id
+##  - get worlds based on current user
+## A world has the following properties:
+##  - name (string)
+##  - id (int)
+##  - user_id (int)
