1fcd9ca070960ea5b26db291dc6e15dba2468663
Paul Veltheim <noreply>
2024-01-23 16:14:46 +0200

    added file play.py

---
+++ play.py
@@ -0,0 +1,6 @@
+## This module handles live playing of worlds
+## This includes the following functionality:
+##  - load world for playing
+##  - start and quit playing
+##  - update global events
+##  - start/end dialogue with npc
